102f4e2aa0d42fccaf92a7be937c7b5bbf785197
bartoszbinda <janisze@outlook.com>
2016-02-19 18:05:12 +0000

    Modified application.rb, via http://stackoverflow.com/questions/8146478/using-html5-video-tag-in-a-simple-rails-app

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
 
   end
 end
