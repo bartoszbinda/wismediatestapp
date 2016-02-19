d0c09094ba5538291dd565fa1e3e30b818bbdcf1
bartoszbinda <janisze@outlook.com>
2016-02-19 03:22:25 +0000

    Added all posts to navbar, shows only 3 posts on main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @articles = Article.order(created_at: :desc)
-    @news = []
+    
     @counter = 0
-    until (@counter < 2) do 
-      @news.push(@articles[@counter])
-      @counter += 1
-    end
+    
     
   end
 end
