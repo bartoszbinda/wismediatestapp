51821b815be85bf7c172518acb3727a326556849
bartoszbinda <janisze@outlook.com>
2016-02-19 01:52:15 +0000

    Remove problem which was caused by using unique constraint on email in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :nick, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
 end
