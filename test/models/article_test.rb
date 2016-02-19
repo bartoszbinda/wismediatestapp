f9fd8c857de1f5151af060937a324ffabef48dd6
bartoszbinda <janisze@outlook.com>
2016-02-19 01:55:45 +0000

    Started writing tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
-  
+   
   test "should not be blank" do
-    @article = Article.new
-    @article.title = ""
+    article = Article.new
+    assert_not article.valid?
+  end
+  test 'title should not be nil' do
+    article = Article.new
+    article.title = ""
     assert_not article.valid?
   end
 end
