6c836198a7d59cf4443a064e36c51301c30586eb
bartoszbinda <janisze@outlook.com>
2016-02-19 15:14:38 +0000

    Fixed issue with setting default_url in paper clip

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: nil 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :title, presence:true, length: { minimum:5, maximum: 200}
 end
