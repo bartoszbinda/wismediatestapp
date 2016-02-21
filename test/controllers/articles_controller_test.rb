47caf33396f606ae1b10539511d0c9a4ffe3a2dc
bartoszbinda <janisze@outlook.com>
2016-02-21 14:58:49 +0000

    added should create article test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -6,4 +6,13 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:articles)
   end
+  
+  test "should create article" do
+    assert_difference('Article.count') do
+      post :create, article: {title: 'Some title'}
+    end
+ 
+    assert_redirected_to article_path(assigns(:article))
+  end
+  
 end
